ba51ef6af2109485352039d9738b745a585e866d
Jainam116 <kharajaynam@gmail.com>
2026-03-10 00:34:08 +0530

    FIx Resume Download Error

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -132,7 +132,7 @@ export function Hero() {
               </Button>
               <Button variant="outline" size="lg" asChild>
                 <a
-                  href="/resume.pdf"
+                  href="/Jainam_Khara_Resume.pdf"
                   download="Jainam_Khara_Resume.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
